d0aec5c14a41b91ecd42164957b99120a760f267
Temzu <40913z@mail.ru>
2023-08-04 11:21:57 +0300

    Initial commit

---
+++ src/main/java/com/temzu/cafefresh/CafefreshApplication.java
@@ -0,0 +1,13 @@
+package com.temzu.cafefresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafefreshApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CafefreshApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/temzu/cafefresh/CafefreshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temzu.cafefresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafefreshApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
